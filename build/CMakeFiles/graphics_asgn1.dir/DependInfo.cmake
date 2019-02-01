a1589f7b7edb6edc0702eb7bcda0c39380141c54
faixan-khan <csfaizankhane@gmail.com>
2019-02-02 01:22:46 +0530

    2nd

---
+++ build/CMakeFiles/graphics_asgn1.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/faizan/Airplane-3d/src/barrel.cpp" "/home/faizan/Airplane-3d/build/CMakeFiles/graphics_asgn1.dir/src/barrel.cpp.o"
+  "/home/faizan/Airplane-3d/src/boat.cpp" "/home/faizan/Airplane-3d/build/CMakeFiles/graphics_asgn1.dir/src/boat.cpp.o"
+  "/home/faizan/Airplane-3d/src/camera.cpp" "/home/faizan/Airplane-3d/build/CMakeFiles/graphics_asgn1.dir/src/camera.cpp.o"
+  "/home/faizan/Airplane-3d/src/color.cpp" "/home/faizan/Airplane-3d/build/CMakeFiles/graphics_asgn1.dir/src/color.cpp.o"
+  "/home/faizan/Airplane-3d/src/cuboid.cpp" "/home/faizan/Airplane-3d/build/CMakeFiles/graphics_asgn1.dir/src/cuboid.cpp.o"
+  "/home/faizan/Airplane-3d/src/input.cpp" "/home/faizan/Airplane-3d/build/CMakeFiles/graphics_asgn1.dir/src/input.cpp.o"
+  "/home/faizan/Airplane-3d/src/main.cpp" "/home/faizan/Airplane-3d/build/CMakeFiles/graphics_asgn1.dir/src/main.cpp.o"
+  "/home/faizan/Airplane-3d/src/monster.cpp" "/home/faizan/Airplane-3d/build/CMakeFiles/graphics_asgn1.dir/src/monster.cpp.o"
+  "/home/faizan/Airplane-3d/src/nonedit.cpp" "/home/faizan/Airplane-3d/build/CMakeFiles/graphics_asgn1.dir/src/nonedit.cpp.o"
+  "/home/faizan/Airplane-3d/src/other_handlers.cpp" "/home/faizan/Airplane-3d/build/CMakeFiles/graphics_asgn1.dir/src/other_handlers.cpp.o"
+  "/home/faizan/Airplane-3d/src/sail.cpp" "/home/faizan/Airplane-3d/build/CMakeFiles/graphics_asgn1.dir/src/sail.cpp.o"
+  "/home/faizan/Airplane-3d/src/timer.cpp" "/home/faizan/Airplane-3d/build/CMakeFiles/graphics_asgn1.dir/src/timer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
